e7d547fc833baea0614ebf1ea0dbb9e19da98c68
Mickey374 <asantemichael37483@gmail.com>
2023-03-01 16:56:26 +0100

    Full stack application with backend and front-end working

---
+++ movies/src/test/java/dev/mickey/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mickey.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
